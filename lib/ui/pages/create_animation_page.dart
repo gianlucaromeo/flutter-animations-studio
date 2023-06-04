5463143d43a60ae428aa6b92aca0852473784805
Gianluca Romeo <gi.romeo@reply.it>
2023-06-04 15:38:18 +0200

    Added scrollbars to home page

--- lib/ui/pages/create_animation_page.dart
+++ lib/ui/pages/create_animation_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter_animations_studio/ui/panels/code_preview_panel.dart';
 import 'package:flutter_animations_studio/ui/panels/configuration_panel.dart';
 import 'package:flutter_animations_studio/ui/panels/controller_panel.dart';
 
-class CreateAnimationPage extends StatelessWidget {
+class CreateAnimationPage extends StatefulWidget {
   const CreateAnimationPage({
     Key? key,
     required this.title,
@@ -18,30 +18,111 @@ class CreateAnimationPage extends StatelessWidget {
   final ConfigurationPanel configurationPanel; // Left Panel
   final AnimationPreviewPanel animationPreviewPanel; // Center (Top) Panel
   final ControllerPanel controllerPanel; // Center (Bottom) Panel
-  final CodePreviewPanel codePreviewPanel; // Right Panel
+  final CodePreviewPanel codePreviewPanel;  // Right Panel
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  State<CreateAnimationPage> createState() => _CreateAnimationPageState();
+}
+
+class _CreateAnimationPageState extends State<CreateAnimationPage> {
+  final minWidth = 1450.0;
+  final minHeight = 800.0;
+
+  final ScrollController _horizontalController = ScrollController();
+  final ScrollController _verticalController = ScrollController();
+
+  @override
+  void dispose() {
+    _horizontalController.dispose();
+    _verticalController.dispose();
+    super.dispose();
+  }
+
+  Scaffold _buildScaffold() {
+    return  Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Row(
         mainAxisSize: MainAxisSize.max,
         children: [
-          configurationPanel,
+          widget.configurationPanel,
           Flexible(
             flex: 0,
             child: Column(
               children: [
-                Expanded(flex: 2, child: animationPreviewPanel),
-                Expanded(child: controllerPanel),
+                Expanded(flex: 2, child: widget.animationPreviewPanel),
+                Expanded(child: widget.controllerPanel),
               ],
             ),
           ),
-          Expanded(child: codePreviewPanel),
+          Expanded(child: widget.codePreviewPanel),
         ],
       ),
+    );
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final scaffold = _buildScaffold();
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final horizontalScrollbarEnabled = constraints.minWidth < minWidth;
+        final verticalScrollbarEnabled = constraints.minHeight < minHeight;
+
+        if (horizontalScrollbarEnabled && verticalScrollbarEnabled) {
+          return Scrollbar(
+            controller: _horizontalController,
+            child: Scrollbar(
+              // IMPORTANT: this scrollbar only handle notification of the vertical ScrollView.
+              // The first ScrollView (depth = 0), is the horizontal one.
+              // The second ScrollView (depth = 1), is the vertical one.
+              // If notification.depth != 1, the notification is bubble up to horizontal Scrollbar.
+              notificationPredicate: (notification) => notification.depth == 1,
+              controller: _verticalController,
+              child: SingleChildScrollView(
+                controller: _horizontalController,
+                scrollDirection: Axis.horizontal,
+                child: SingleChildScrollView(
+                  controller: _verticalController,
+                  child: SizedBox(
+                    width: minWidth,
+                    height: minHeight,
+                    child: scaffold,
+                  ),
+                ),
+              ),
+            ),
+          );
+        } else if (horizontalScrollbarEnabled) {
+          return Scrollbar(
+            controller: _horizontalController,
+            child: SingleChildScrollView(
+              controller: _horizontalController,
+              scrollDirection: Axis.horizontal,
+              child: SizedBox(
+                width: minWidth,
+                child: scaffold,
+              ),
+            ),
+          );
+        } else if (verticalScrollbarEnabled) {
+          return Scrollbar(
+            controller: _verticalController,
+            child: SingleChildScrollView(
+              controller: _verticalController,
+              child: SizedBox(
+                height: minHeight,
+                child: scaffold,
+              ),
+            ),
+          );
+        }
+
+        return scaffold;
+      },
     );
   }
 }
