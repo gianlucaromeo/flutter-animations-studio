1e2ca3b9c6f8932930e0bcd2055796e4e5310358
Gianluca Romeo <gianlucaromeo@outlook.com>
2023-11-19 15:56:06 +0100

    Added code preview panel's placeholder

--- lib/ui/panels/code_preview_panel.dart
+++ lib/ui/panels/code_preview_panel.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations_studio/extensions.dart';
+import 'package:flutter_animations_studio/ui/theme.dart';
 
 class CodePreviewPanel extends StatelessWidget {
   const CodePreviewPanel({Key? key}) : super(key: key);
@@ -10,12 +12,22 @@ class CodePreviewPanel extends StatelessWidget {
       height: double.infinity,
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.surface,
-        border: const Border(
-          left: BorderSide(
-            color: Colors.white,
-            width: 0.3,
+        borderRadius: AppBorderRadius.large,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.warning_amber),
+          12.0.verticalSpace,
+          const Text(
+            "Work in progress",
           ),
-        ),
+          12.0.verticalSpace,
+          const Text(
+            "This panel will contain the Dart code of the animation",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ],
       ),
     );
   }
